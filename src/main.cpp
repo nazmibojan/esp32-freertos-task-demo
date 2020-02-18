d66abc530378f1afda4b52830c28d4d2c83021c4
Nazmi Febrian <nazmi.febrian@gmail.com>
2020-02-18 15:11:32 +0700

    Add event bits between task

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
+#define ALL_EVENT_BITS 0xFF
+#define WIFI_CONNECTION_BIT (0x01UL)
+
 const char *ssid = "CONNEXT-AXIATA";
-const char *pass = "4xiatadigitalllabs18";
+const char *pass = "4xiatadigitallabs18";
 int lastRequest = 0;
+
 TaskHandle_t uartTaskHandle;
 TaskHandle_t ledTaskHandle;
+EventGroupHandle_t sampleGroup = NULL;
 
-void reconnect();
+void setupWifi();
 void uartTask(void *parameter);
 void ledTask(void *parameter);
 
@@ -15,20 +20,27 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(BUILTIN_LED, OUTPUT);
   Serial.begin(9600);
-  delay(1000);
+  vTaskDelay(1000);
+
+  sampleGroup = xEventGroupCreate();
+  configASSERT(sampleGroup);
 
   // Create RTOS task
-  xTaskCreatePinnedToCore(uartTask, "Uart Task", 2048, NULL, 1, &uartTaskHandle,
+  xTaskCreatePinnedToCore(uartTask, "UART Task", 2048, NULL, 1, &uartTaskHandle,
                           0);
   xTaskCreatePinnedToCore(ledTask, "LED Task", 2048, NULL, 2, &ledTaskHandle,
                           0);
+
+  setupWifi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (millis() - lastRequest > 60000) {
+  if (millis() - lastRequest > 10000) {
     if (WiFi.status() != WL_CONNECTED) {
-      reconnect();
+      setupWifi();
+    } else {
+      Serial.println("WiFi is already connected...");
     }
 
     lastRequest = millis();
@@ -36,24 +48,50 @@ void loop() {
 }
 
 void uartTask(void *parameter) {
+  EventBits_t clientBits;
+
   for (;;) {
-    Serial.println("Tes uart first...");
-    delay(3000);
-    Serial.println("Test uart second...");
-    delay(3000);
+    clientBits = xEventGroupWaitBits(sampleGroup, ALL_EVENT_BITS, pdTRUE,
+                                     pdFALSE, 1000 / portTICK_PERIOD_MS);
+    clientBits &= WIFI_CONNECTION_BIT;
+
+    if (clientBits != 0) {
+      Serial.println("WiFi Connected...");
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+    }
   }
 }
 
 void ledTask(void *parameter) {
   for (;;) {
-    digitalWrite(BUILTIN_LED, HIGH);
-    delay(3000);
-    digitalWrite(BUILTIN_LED, LOW);
-    delay(3000);
+    if (WiFi.status() == WL_CONNECTED) {
+      digitalWrite(BUILTIN_LED, HIGH);
+      vTaskDelay(1000);
+      digitalWrite(BUILTIN_LED, LOW);
+      vTaskDelay(1000);
+    } else {
+      digitalWrite(BUILTIN_LED, LOW);
+    }
   }
 }
 
-void reconnect() {
+void setupWifi() {
+  vTaskDelay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
   WiFi.begin(ssid, pass);
-  Serial.println("WiFi connected...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    vTaskDelay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+  xEventGroupSetBits(sampleGroup, WIFI_CONNECTION_BIT);
 }
